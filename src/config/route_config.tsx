7443f59fdfd9543dfa57e3691a24ea45c28157ce
Nguyen Son Tung <sontung2000ct@gmail.com>
2026-01-02 11:01:29 +0700

    feat: update router configuration to enable new future features in React Router v7

--- src/config/route_config.tsx
+++ src/config/route_config.tsx
@@ -106,6 +106,13 @@ export const routerConfig = createBrowserRouter(
     },
   ],
   {
-    future: {},
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
   }
 );
