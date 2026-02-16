0c44c9f9b978e62c876466fb734b062c30b6c853
nstdev1001 <sontung2000ct@gmail.com>
2026-02-16 21:21:57 +0700

    feat: update AboutCompany component with new image and improved typing configuration for enhanced presentation

--- src/pages/Home/AboutCompany/index.tsx
+++ src/pages/Home/AboutCompany/index.tsx
@@ -10,38 +10,37 @@ const AboutCompany = () => {
     () => [
       `<span class='font-semibold'>TFORART – Cùng Nhau Kiến Tạo Nghệ Thuật</span><br/><br/>Ra đời vào năm 2024, Tforat mang trong mình sứ mệnh kết nối nghệ thuật, sáng tạo và công nghệ, tạo nên những giá trị thẩm mỹ vượt thời gian. Chúng tôi hoạt động trong lĩnh vực nghệ thuật, media và sáng tạo nội dung, không ngừng đổi mới để mang đến những giải pháp sáng tạo chuyên nghiệp, giúp khách hàng khẳng định dấu ấn riêng trong thế giới truyền thông số.<br/><br/>Với phương châm "Together We Create Art", chúng tôi tin rằng nghệ thuật không chỉ là cái đẹp, mà còn là sự kết nối giữa con người, cảm xúc và câu chuyện. Từng sản phẩm mà TFORART tạo ra không chỉ đáp ứng tiêu chuẩn chất lượng cao mà còn chứa đựng tâm huyết, sự tinh tế và giá trị sáng tạo độc nhất.`,
     ],
-    []
+    [],
   );
 
   // Memoize typing parameters to keep them stable
   const typingConfig = useMemo(
     () => ({
-      typeSpeed: 5,
+      typeSpeed: 1,
       backSpeed: 0,
-      cursorChar: "|",
+      cursorChar: "",
       loop: false,
     }),
-    []
+    [],
   );
 
   return (
     <Fragment>
-      <div className="about-wrapper flex flex-col md:flex-col lg:flex-row justify-center gap-20 mx-auto">
+      <div className="about-wrapper flex flex-col justify-center gap-20">
         <motion.img
-          className="md:max-w-[600px] lg:max-w-[600px] h-auto rounded-ee-2xl bg-top object-cover mx-auto"
-          src="/tforart_homepage_color.jpg"
+          className=" h-auto rounded-ee-2xl bg-top object-cover mx-auto"
+          src="/tforart_studio_white.jpg"
           alt="Tforart team image"
           {...defaultYMotionProps}
           initial={{ opacity: 0, y: 100 }}
         />
-
-        <div className="content-about-company md:max-w-[600px] flex flex-col items-start gap-5">
+        <div className="content-about-company flex flex-col items-start gap-5">
           <motion.h1
             className={`text-5xl font-semibold ${styles.gradientText}`}
             {...defaultYMotionProps}
           >
             <span className="font-thin">About</span>
-            <p className="">&apos; TFORART &apos;</p>
+            <p>&apos; TFORART &apos;</p>
           </motion.h1>
           <motion.div
             className="text-md flex flex-col gap-3"
